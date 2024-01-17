537b4ecea5e8b28df17598d64ab0f499cfe6fd16
sciva-fe <scifede4@email.com>
2024-01-17 10:37:50 +0100

    The Old @Effect Decorator & Registering Effects

--- src/app/store/counter.effects.ts
+++ src/app/store/counter.effects.ts
@@ -1,19 +1,25 @@
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { decrement, increment } from "./counter.actions";
 import { tap } from "rxjs";
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class CounterEffects {
-        saveCount = createEffect(
-        () => 
-            this.actions$.pipe(
-                ofType(increment, decrement),
-                tap((action) => {
-                    console.log(action);
-                    localStorage.setItem('count', action.value.toString())
-                })
-        ),
-        {dispatch: false}
-    );
+    //@Effect({dispatch: false})
+    saveCount = createEffect(
+    () => 
+        this.actions$.pipe(
+            ofType(increment, decrement),
+            tap((action) => {
+                console.log(action);
+                localStorage.setItem('count', action.value.toString())
+            })
+    ),
+    {dispatch: false});
 
     constructor(private actions$: Actions) {}
+}
+
+function Effect(): (target: CounterEffects, propertyKey: "saveCount") => void {
+    throw new Error("Function not implemented.");
 }
